e15bd74b88e00a131194358dc7acd80470638de6
dnosko <dasa.noskova@gmail.com>
2020-12-16 15:29:47 +0100

    r5, c0

---
+++ alfabet.c
@@ -0,0 +1,21 @@
+/*
+ * alfabet.c
+ *
+ *  Created on: 15.12.2020
+ *      Author: PC-15
+ */
+
+#ifndef SOURCES_ALFABET_C_
+#define SOURCES_ALFABET_C_
+
+
+
+/*char X[] = {
+B01000100, //stlpec
+B00101000,
+B00010000,
+B00101000,
+B01000100
+};*/
+
+#endif /* SOURCES_ALFABET_C_ */
